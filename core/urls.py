4f0c675af8830943eb89ebe383de072244f8f019
wchavers <wachavers@quinnipiac.edu>
2015-09-22 19:06:52 +0000

    Add question create view

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
-          )
+    url(r'^question/create/$', QuestionCreateView.as_view(), name='question_create'),
+)
